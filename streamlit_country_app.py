c17b8dc45bcdc2fbc2b982e2a9feddd757cd9137
DriesFaems <76656102+DriesFaems@users.noreply.github.com>
2024-03-07 09:21:31 +0100

    Add files via upload

--- streamlit_country_app.py
+++ streamlit_country_app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Load data
 
-df = pd.read_excel('Aggregate country.xlsx')
+df = pd.read_excel('Aggregate country with country.xlsx')
 
 df_long = df
 
@@ -13,21 +13,21 @@ st.title('European Scaleup Monitor: Bechmarking of countries in Europe')
 
 # add subheader
 
-st.subheader('This benchmarking tool allows you to compare different countries on different growth metrics. For more information on the European Scaleup Institute, visit https://scaleupinstitute.eu/. For more information on this benchmark tool, please reach out to Dries Faems (https://www.linkedin.com/in/dries-faems-0371569/)')
+st.subheader('This benchmarking tool allows you to compare different countries on different growth metrics. For more information on the European Scaleup Institute, visit https://scaleupinstitute.eu/. For more information on this benchmark tool, please reach out to info@scaleupinstitute.eu')
 
 # Country selection
-countries = df_long['Country ISO code'].unique()
+countries = df_long['Country'].unique()
 selected_countries = st.multiselect('Select countries', countries, default=countries[0])
 
-metrics = ['Scaler (companies with compound annual growth rate of 10% in past three years)', 'HighGrowthFirm (companies with compound annual growth rate of 20% in past three years)', 'Consistent HighGrowthFirm (companies that grew 20% in at least 2 of the past three years)', 'Consistent Hypergrower (companies that grew 40% in at least 2 of the past three years)', 'Gazelle (consistent high growth firm that is younger than 10 years)', 'Mature HighGrowthFirm (consistent high growth firm that is older than 10 years)', 'Scaleup (consistent hypergrower that is younger than 10 years)', 'Superstar (consistent hypergrower that is older than 10 years)' ]
+metrics = ['Scaler (companies with average annual growth rate of 10% in past three years)', 'HighGrowthFirm (companies with average annual growth rate of 20% in past three years)', 'Consistent HighGrowthFirm (high growth companies that grew 20% in at least 2 of the past three years)', 'Consistent Hypergrower (high growth companies that grew 40% in at least 2 of the past three years)', 'Gazelle (consistent high growth firm that is younger than 10 years)', 'Mature HighGrowthFirm (consistent high growth firm that is older than 10 years)', 'Scaleup (consistent hypergrower that is younger than 10 years)', 'Superstar (consistent hypergrower that is older than 10 years)' ]
 selected = st.selectbox('Select metrics', metrics)
-if selected == 'Scaler (companies with compound annual growth rate of 10% in past three years)':
+if selected == 'Scaler (companies with average annual growth rate of 10% in past three years)':
     selected_metrics = 'Scaler'
-if selected == 'HighGrowthFirm (companies with compound annual growth rate of 20% in past three years)':
+if selected == 'HighGrowthFirm (companies with average annual growth rate of 20% in past three years)':
     selected_metrics = 'HighGrowthFirm'
-if selected == 'Consistent HighGrowthFirm (companies that grew 20% in at least 2 of the past three years)':
+if selected == 'Consistent HighGrowthFirm (high growth companies that grew 20% in at least 2 of the past three years)':
     selected_metrics = 'ConsistentHighGrowthFirm'
-if selected == 'Consistent Hypergrower (companies that grew 40% in at least 2 of the past three years)':
+if selected == 'Consistent Hypergrower (high growth companies that grew 40% in at least 2 of the past three years)':
     selected_metrics = 'VeryHighGrowthFirm'
 if selected == 'Gazelle (consistent high growth firm that is younger than 10 years)':
     selected_metrics = 'Gazelle'
@@ -40,7 +40,7 @@ if selected == 'Superstar (consistent hypergrower that is older than 10 years)':
 
 
 # Filtering data
-filtered_data = df_long[df_long['Country ISO code'].isin(selected_countries)]
+filtered_data = df_long[df_long['Country'].isin(selected_countries)]
 number_of_countries = len(selected_countries)
 
 clicked = st.button('Show data')
@@ -51,7 +51,7 @@ if clicked:
     x = [2018, 2019, 2020, 2021, 2022]
     ylistmeta = []
     for country in selected_countries:
-        country_data = filtered_data[filtered_data['Country ISO code'] == country]
+        country_data = filtered_data[filtered_data['Country'] == country]
         ylist = list()
         ylist.append(country_data[selected_metrics + ' ' + str(2018) + ' %'].iloc[0]*100)
         ylist.append(country_data[selected_metrics + ' ' + str(2019) + ' %'].iloc[0]*100)
@@ -81,6 +81,38 @@ if clicked:
     ax.set_ylabel(selected_metrics+ ' %')
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15))
     st.pyplot(fig)
+
+    listrelevantcolumns = ['Country']
+    for i in range(len(filtered_data.columns)):
+        if selected_metrics in filtered_data.columns[i]:
+            listrelevantcolumns.append(filtered_data.columns[i])
+    newtable = filtered_data[listrelevantcolumns]
+
+    # change column names
+
+    newtable.columns = newtable.columns.to_series().replace(selected_metrics + ' 2022 Obs', 'Total number of observed companies in 2022') \
+    .replace(selected_metrics + ' 2022 Num', 'Number of ' + selected_metrics + 's in 2022') \
+    .replace(selected_metrics + ' 2022 %', 'Percentage of ' + selected_metrics + 's in 2022') \
+    .replace(selected_metrics + ' 2021 Obs', 'Total number of observed companies in 2021') \
+    .replace(selected_metrics + ' 2021 Num', 'Number of ' + selected_metrics + 's in 2021') \
+    .replace(selected_metrics + ' 2021 %', 'Percentage of ' + selected_metrics + 's in 2021') \
+    .replace(selected_metrics + ' 2020 Obs', 'Total number of observed companies in 2020') \
+    .replace(selected_metrics + ' 2020 Num', 'Number of ' + selected_metrics + 's in 2020') \
+    .replace(selected_metrics + ' 2020 %', 'Percentage of ' + selected_metrics + 's in 2020') \
+    .replace(selected_metrics + ' 2019 Obs', 'Total number of observed companies in 2019') \
+    .replace(selected_metrics + ' 2019 Num', 'Number of ' + selected_metrics + 's in 2019') \
+    .replace(selected_metrics + ' 2019 %', 'Percentage of ' + selected_metrics + 's in 2019') \
+    .replace(selected_metrics + ' 2018 Obs', 'Total number of observed companies in 2018') \
+    .replace(selected_metrics + ' 2018 Num', 'Number of ' + selected_metrics + 's in 2018') \
+    .replace(selected_metrics + ' 2018 %', 'Percentage of ' + selected_metrics + 's in 2018')
+
+    # set index to country
+
+    newtable = newtable.set_index('Country')
+
+    #print newtable as table in streamlit
+
+    st.write(newtable)
 
 else:
     st.write('Click to show data')
